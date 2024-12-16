b6e2544cda3ec977210966dd280cfd811ed0e804
ss5579 <130932847+ss5579@users.noreply.github.com>
2024-12-16 23:42:13 +0600

    Методы классов 2

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class Calculator {
+    int a;
+    int b;
+
+    public void addition() {
+        System.out.println(a + b);
+    }
+    public void subtraction() {
+        System.out.println(a - b);
+    }
+    public void division() {
+        System.out.println(a / b);
+    }
+    public void multiplication() {
+        System.out.println(a * b);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.a = 8;
+        calculator.b = 2;
+        calculator.addition();
+        calculator.subtraction();
+        calculator.division();
+        calculator.multiplication();
+    }
+}
